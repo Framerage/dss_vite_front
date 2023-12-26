3e4d932a35475f50889282f28fd4e8b2be2c4522
framerage <officialigonin@mail.ru>
2023-12-26 17:37:12 +0400

    add form item component

--- src/pages/indivOrderPage/IndivOrderPage.tsx
+++ src/pages/indivOrderPage/IndivOrderPage.tsx
@@ -1,16 +1,62 @@
 import React from "react";
-import {useForm} from "react-hook-form";
+import {RegisterOptions, UseFormRegister, useForm} from "react-hook-form";
 
 import ImageAdder from "components/imageAdder";
+import {useSelector} from "react-redux";
+import {selectUserData} from "store/modules/auth/selectors";
 import classes from "./indivOrderPage.module.css";
 
 type ImageType = {fileBody: string; fileName: string; id: number};
 interface IndivOrderFormData {
   orderDescrip: string | null;
   images: ImageType[];
+  email: string;
+  name: string;
+  phoneNum: string;
+  city: string;
 }
+interface IndivOrderFormItemProps {
+  itemName: keyof IndivOrderFormData;
+  inputType?: string;
+  itemPlaceHolder?: string;
+  parentStyle?: string;
+  childStyle?: string;
+  errorText?: string;
+  defaultValue?: string;
+  register: UseFormRegister<IndivOrderFormData>;
+  registerOprions?:
+    | RegisterOptions<IndivOrderFormData, keyof IndivOrderFormData>
+    | undefined;
+}
+const IndivOrderFormItem: React.FC<IndivOrderFormItemProps> = ({
+  register,
+  itemName,
+  itemPlaceHolder,
+  parentStyle,
+  childStyle,
+  errorText,
+  defaultValue,
+  registerOprions,
+  inputType = "text",
+}) => {
+  return (
+    <div className={parentStyle}>
+      <input
+        className={childStyle}
+        type={inputType}
+        {...register(itemName, {...registerOprions})}
+        name={itemName}
+        placeholder={itemPlaceHolder || ""}
+        defaultValue={defaultValue}
+      />
+      {errorText && <span>{errorText}</span>}
+    </div>
+  );
+};
 const minDescripTextLength = 20;
+
 const IndivOrderPage: React.FC = () => {
+  const curUser = useSelector(selectUserData);
   const {register, handleSubmit, setValue, formState, watch} =
     useForm<IndivOrderFormData>({
       mode: "onSubmit",
@@ -19,6 +65,10 @@ const IndivOrderPage: React.FC = () => {
       values: {
         orderDescrip: null,
         images: [],
+        email: curUser?.email || "",
+        name: curUser?.name || "",
+        phoneNum: "",
+        city: "",
       },
     });
   const orderDescripRequiredError =
@@ -40,6 +90,34 @@ const IndivOrderPage: React.FC = () => {
         onSubmit={handleSubmit(onCreateIndivOrder)}
         style={{display: "flex", flexDirection: "column"}}
       >
+        <div>
+          <div>
+            <IndivOrderFormItem
+              itemName="name"
+              defaultValue={curUser?.name}
+              itemPlaceHolder="введите свой ник"
+              register={register}
+            />
+            <IndivOrderFormItem
+              itemName="email"
+              defaultValue={curUser?.email}
+              itemPlaceHolder="введите свою почту"
+              register={register}
+            />
+          </div>
+          <div>
+            <IndivOrderFormItem
+              itemName="phoneNum"
+              itemPlaceHolder="введите номер телефона"
+              register={register}
+            />
+            <IndivOrderFormItem
+              itemName="city"
+              itemPlaceHolder="введите свой город"
+              register={register}
+            />
+          </div>
+        </div>
         <span>Описание заказа</span>
         <textarea
           {...register("orderDescrip", {
--- src/pages/orderPage/OrderPage.tsx
+++ src/pages/orderPage/OrderPage.tsx
@@ -1,7 +1,7 @@
 import React, {useCallback, useEffect} from "react";
 import {shallowEqual, useDispatch, useSelector} from "react-redux";
 
-import {useForm} from "react-hook-form";
+import {RegisterOptions, UseFormRegister, useForm} from "react-hook-form";
 import {useNavigate} from "react-router-dom";
 import {APP_AUTH_ROUTES} from "utils/routes";
 import Cookies from "js-cookie";
@@ -12,7 +12,10 @@ import {
   selectOrderCreatingError,
   selectOrderCreatingIsLoading,
 } from "store/modules/order/selectors";
-import {selectShopCart} from "store/modules/cart/selectors";
+import {
+  selectShopCart,
+  selectShopCartIsLoading,
+} from "store/modules/cart/selectors";
 import {selectUserData} from "store/modules/auth/selectors";
 import {resetOrderCreatingResult} from "store/modules/order/actions";
 
@@ -22,6 +25,8 @@ import {
 } from "store/modules/auth/async-actions";
 import {resetUserShopCart} from "store/modules/cart/actions";
 import {EMAIL_PATTERN, PHONE_NUM_PATTERN} from "constants/appConstants";
+import {getUserShopCart} from "store/modules/cart/async-actions";
+import PointLoader from "components/pointLoader";
 import classes from "./orderPage.module.css";
 
 interface OrderFormData {
@@ -30,23 +35,72 @@ interface OrderFormData {
   phoneNum: string;
   city: string;
 }
+interface OrderFormItemProps {
+  itemName: keyof OrderFormData;
+  inputType?: string;
+  itemPlaceHolder?: string;
+  parentStyle?: string;
+  childStyle?: string;
+  errorText?: string;
+  defaultValue?: string;
+  register: UseFormRegister<OrderFormData>;
+  registerOprions?:
+    | RegisterOptions<OrderFormData, keyof OrderFormData>
+    | undefined;
+}
+
+const OrderFormItem: React.FC<OrderFormItemProps> = ({
+  register,
+  itemName,
+  itemPlaceHolder,
+  parentStyle,
+  childStyle,
+  errorText,
+  defaultValue,
+  registerOprions,
+  inputType = "text",
+}) => {
+  return (
+    <div className={parentStyle}>
+      <input
+        className={childStyle}
+        type={inputType}
+        {...register(itemName, {...registerOprions})}
+        name={itemName}
+        placeholder={itemPlaceHolder || ""}
+        defaultValue={defaultValue}
+      />
+      {errorText && <span>{errorText}</span>}
+    </div>
+  );
+};
+
 const OrderPage: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
   const orderRequest = useSelector(selectOrderCreating);
   const orderRequestIsLoading = useSelector(selectOrderCreatingIsLoading);
   const orderRequestError = useSelector(selectOrderCreatingError);
+  const shopCartIsLoading = useSelector(selectShopCartIsLoading);
   const {handleSubmit, register, formState} = useForm<OrderFormData>({
     mode: "onSubmit",
     reValidateMode: "onSubmit",
-    shouldFocusError: false,
+    shouldFocusError: true,
   });
 
   const emailPatternError = formState.errors?.email?.type === "pattern";
+  const nameError = formState.errors.name?.type === "required";
+  const emailError = formState.errors.email?.type === "required";
   const telNumberPatternError = formState.errors?.phoneNum?.type === "pattern";
   const minLengthPhoneNumberError =
     formState.errors?.phoneNum?.type === "minLength";
-
+  const telNumberErrTextReqPattern = telNumberPatternError
+    ? "Неверный формат"
+    : "Обязательно для заполнения";
+  const telNumberLengthErrText = minLengthPhoneNumberError
+    ? "Минимальное количество символов 12"
+    : telNumberErrTextReqPattern;
+  const emailErrorText = emailError ? "Обязательно для заполнения" : "";
   const cartList = useSelector(selectShopCart, shallowEqual);
   const curUser = useSelector(selectUserData);
   const totalPrice = cartList.reduce(
@@ -54,6 +108,18 @@ const OrderPage: React.FC = () => {
     0,
   );
   const accS = Cookies.get("perAcTkn");
+
+  useEffect(() => {
+    if (!cartList?.length && accS && curUser) {
+      dispatch(
+        getUserShopCart({
+          cards: curUser.userCart,
+          auth: accS,
+          email: curUser.email,
+        }),
+      );
+    }
+  }, []);
 
   useEffect(() => {
     if (!orderRequest) {
@@ -109,69 +175,70 @@ const OrderPage: React.FC = () => {
       ) : (
         <div className={classes.itemsContainer}>
           <div className={classes.formItems}>
-            <div className={classes.formItem}>
-              <input
-                type="text"
-                {...register("name")}
-                defaultValue={curUser?.name}
-                name="name"
-                placeholder="Ваше имя/ник на сайте"
-                required
-              />
-            </div>
-            <div className={classes.formItem}>
-              <input
-                type="text"
-                {...register("email", {pattern: EMAIL_PATTERN})}
-                defaultValue={curUser?.email}
-                name="email"
-                placeholder="Ваша почта"
-                required
-              />
-              {emailPatternError && <span>Неверный формат</span>}
-            </div>
+            <OrderFormItem
+              itemName="name"
+              itemPlaceHolder="Ваше имя/ник на сайте"
+              register={register}
+              registerOprions={{
+                required: true,
+              }}
+              parentStyle={classes.formItem}
+              childStyle={classes.inputFormItem}
+              errorText={nameError ? "Обязательно заполните ник" : ""}
+              defaultValue={curUser?.name}
+            />
+            <OrderFormItem
+              itemName="email"
+              itemPlaceHolder="Ваша почта"
+              register={register}
+              registerOprions={{
+                pattern: EMAIL_PATTERN,
+                required: true,
+              }}
+              parentStyle={classes.formItem}
+              childStyle={classes.inputFormItem}
+              errorText={emailPatternError ? "Неверный формат" : emailErrorText}
+              defaultValue={curUser?.email}
+            />
           </div>
 
           <div className={classes.formItems}>
-            <div className={classes.formItem}>
-              <input
-                type="tel"
-                {...register("phoneNum", {
-                  pattern: PHONE_NUM_PATTERN,
-                  minLength: 12,
-                })}
-                name="phoneNum"
-                placeholder="+71234567890"
-                required
-              />
-              {(telNumberPatternError || minLengthPhoneNumberError) && (
-                <span>
-                  {minLengthPhoneNumberError
-                    ? "Минимальное количество символов 12"
-                    : "Неверный формат"}
-                </span>
-              )}
-            </div>
-            <div className={classes.formItem}>
-              <input
-                type="text"
-                {...register("city")}
-                name="city"
-                placeholder="Ваш город"
-              />
-            </div>
+            <OrderFormItem
+              itemName="phoneNum"
+              itemPlaceHolder="+71234567890"
+              register={register}
+              registerOprions={{
+                pattern: PHONE_NUM_PATTERN,
+                minLength: 12,
+                required: true,
+              }}
+              parentStyle={classes.formItem}
+              childStyle={classes.inputFormItem}
+              errorText={telNumberLengthErrText}
+            />
+            <OrderFormItem
+              itemName="city"
+              itemPlaceHolder="Ваш город"
+              register={register}
+              parentStyle={classes.formItem}
+              childStyle={classes.inputFormItem}
+            />
           </div>
         </div>
       )}
 
       <div className={classes.productList}>
-        {cartList.map(item => (
-          <div key={item._id} className={classes.listItem}>
-            <span className={classes.textName}>{item.title}</span>
-            <span>{item.itemCount}&nbsp;шт.</span>
-            <span>{item.price * item.itemCount}&nbsp;руб</span>
-          </div>
-        ))}
+        {shopCartIsLoading ? (
+          <PointLoader scale={0.1} />
+        ) : (
+          cartList.map(item => (
+            <div key={item._id} className={classes.listItem}>
+              <span className={classes.textName}>{item.title}</span>
+              <span>{item.itemCount}&nbsp;шт.</span>
+              <span>{item.price * item.itemCount}&nbsp;руб</span>
+            </div>
+          ))
+        )}
       </div>
 
       <div className={classes.completeItems}>
